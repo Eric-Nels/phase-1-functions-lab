61672454a825d0e72fe9070a925dd37d7410e61c
Eric Nelson <ernelson12o0@gmail.com>
2023-02-21 15:44:50 -0800

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    return Math.abs(42 - block);
+}
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(startBlock,endBlock) {
+    return Math.abs((endBlock - startBlock) * 264);
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    if (distanceTravelledInFeet(startBlock, endBlock) < 400)
+        return 0;
+    else if (distanceTravelledInFeet(startBlock, endBlock) > 400 && distanceTravelledInFeet(startBlock, endBlock) < 2000) 
+        return .02 * (distanceTravelledInFeet(startBlock, endBlock) - 400);
+    else if (distanceTravelledInFeet(startBlock, endBlock) > 2000 && distanceTravelledInFeet(startBlock, endBlock) < 2500)
+        return 25;
+    else 
+        return "cannot travel that far";
+}
